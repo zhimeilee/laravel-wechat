3c1ea5d5a30217256e951cdd91224221c264c61d
Leuko <leuko@123.com>
2015-05-08 00:28:46 +0800

    new

---
+++ src/config/wechat.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+    'default' => array(
+        'token'          => 'token',
+        'encodingaeskey' => 'encodingaeskey',
+        'appid'          => 'appid',
+        'appsecret'      => 'appsecret',
+        'debug'          => false,
+        'logcallback'    => false,
+    )
+);
